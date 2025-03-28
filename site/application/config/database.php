9d4ddb6f8ead2081445d8366b950e35ddf55b807
James R <300089902@fvtc.edu>
2025-03-28 10:05:18 -0500

    Added connection string

--- site/application/config/database.php
+++ site/application/config/database.php
@@ -73,6 +73,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+//Remote
+$db['default'] = array(
+	'dsn'	=> 'mongodb+srv://root:runners@clusterrunners.qaayv9q.mongodb.net/?retryWrites=true&w=majority&appName=ClusterRunners',
+	'hostname' => '',
+	'username' => 'root',
+	'password' => 'runners',
+	'database' => '',
+	'dbdriver' => 'mongodb',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => TRUE,
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+//Local
+/*
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => '127.0.0.1:8001',
@@ -94,3 +119,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+*/
